8a0f0af6e5ee7002205c97cb6e40f3f967c02007
Nicholas Rader <nick.rader@yahoo.com>
2022-06-17 05:20:13 -0600

    yep, that's why I get 4 cc. Case 3 can be true., which trigger build another CC.
    some progress in locating a point2D for natural expansion
    
    2 steps forward, one step back.  Rally worky, attack waves - no worky.
    
    I'm not a senior yet.  s/b StOnEr MomEnT.
    
    cleanup comments

--- build/src/ZappBrannigan.cpp
+++ build/src/ZappBrannigan.cpp
@@ -2,8 +2,9 @@
 
 #include "Historican.h"
 #include "Hub.h"
-#include "strategies/terran/ZappBrannigan.h"
 #include "core/API.h"
+#include "sc2api/sc2_agent.h"
+#include "strategies/terran/ZappBrannigan.h"
 
 namespace {
 Historican gHistory("strategy.ZappBrannigan");
@@ -29,10 +30,10 @@ void Killbots::OnStep(Builder* builder_) {
   }
 
   if (!to_build_or_not_to_build)
-      if (minerals >= 150) {
-          builder_->ScheduleObligatoryOrder(sc2::UNIT_TYPEID::TERRAN_BARRACKS);
-          ++number_of_barracks;
-      }
+    if (minerals >= 150) {
+      builder_->ScheduleObligatoryOrder(sc2::UNIT_TYPEID::TERRAN_BARRACKS);
+      ++number_of_barracks;
+    }
   Strategy::OnStep(builder_);
 }
 
@@ -47,8 +48,22 @@ void Killbots::OnUnitIdle(const sc2::Unit* unit_, Builder* builder_) {
   }
 }
 
-bool Killbots::Should_Build_Expansion() {
+// But we don't execute Attack() reaching attack_limit
 
+void Killbots::OnUnitCreated(const sc2::Unit* unit_, Builder* builder_) {
+  const Expansions& expansions = gHub->GetExpansions();
+  sc2::Point3D natural_expansion = expansions[1].town_hall_location;  // test
+  sc2::Point2D rally(natural_expansion.x, natural_expansion.y);
+
+  switch (unit_->unit_type.ToType()) {
+    case sc2::UNIT_TYPEID::TERRAN_MARINE:
+      sc2::Units units_{};
+      units_.push_back(unit_);
+      gAPI->action().Attack(units_, rally);
+  }
+}
+
+bool Killbots::Should_Build_Expansion() {
   switch (number_of_townhalls) {
     case 1:
       if (number_of_barracks >= 3)
@@ -68,7 +83,7 @@ bool Killbots::Should_Build_Expansion() {
       else
         return false;
       break;
-    default:  // this should mean we only expo to 3 CC total ??? Negative, we keep expanding, it seems...
+    default:  // Duhhh...  If I have 3 CC, then is true, and I build a 4th.
       return false;
       break;
   }
--- src/strategies/terran/ZappBrannigan.h
+++ src/strategies/terran/ZappBrannigan.h
@@ -9,11 +9,15 @@ reached their limit, and shutdown." -Captain Zapp Brannigan */
 struct Killbots : Strategy {
     Killbots();
 
-    void OnGameStart(Builder* builder_) final;
+    void OnGameStart(Builder*) final;
 
-    void OnStep(Builder* builder_) final;
+    void OnStep(Builder*) final;
 
-    void OnUnitIdle(const sc2::Unit* unit_, Builder* builder_) final;
+    void OnUnitIdle(const sc2::Unit*, Builder*) final;
+
+    void OnUnitCreated(const sc2::Unit*, Builder*);
+
+    //void OnUnitEnterVision(const sc2::Unit*, Builder*);
 
 private:
     bool Should_Build_Expansion();
@@ -22,3 +26,7 @@ private:
     int number_of_townhalls = 1; // we start with one command center.
     int number_of_barracks = 0; 
 };
+
+// TODO: set rally point when Barracks is started?constructed?Idle?
+//  Obviously, when it gets called will be important, but for now,
+//  just want to 
